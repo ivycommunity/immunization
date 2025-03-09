751f9ce5532334bd28716bfca40e819980e94940
mumbiann <107537497+mumbiann@users.noreply.github.com>
2025-03-09 22:52:40 +0300

    Appointments seeder

--- backend/app/Models/Appointment.php
+++ backend/app/Models/Appointment.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Appointment extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'baby_id',
+        'guardian_id',
+        'vaccine_id',
+        'doctor_id',
+        'appointment_date',
+        'status',
+        'reminder_sent',
+        'notes',
+    ];
+
+    
+    public function baby()
+    {
+        return $this->belongsTo(Baby::class);
+    }
+
+    
 }
